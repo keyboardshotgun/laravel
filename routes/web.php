ce96cfca5fbf6e8c3fb6a975ca91516e89d2880b
상현 <interse@naver.com>
2020-10-29 07:52:02 +0900

    update controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController
+use App\Http\Controllers\WelcomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
