465e68a1cbc84391660dfa0a41eae0deb318c5c3
상현 <interse@naver.com>
2020-10-29 07:11:45 +0900

    update models

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,4 +9,5 @@ class Person extends Model
 {
     use HasFactory;
     protected $table = 'persons'; // 첫 줄에 처리해줘야 함.
+    public $timestamps = false; // 라라벨 tinker는 삽입시 데이터 컬럼명이 updated_at 이여야 하는데 , 그러지 않은 경우위해
 }
