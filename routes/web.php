b5be22653e6353b1e3ecf815dc78f734d7a7034f
상현 <interse@naver.com>
2020-10-29 07:51:42 +0900

    update controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', WelcomeController@index);
 
 
 Route::get('/test', function () {
