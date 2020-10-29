50c7fd2dfdf1d38d2bb77e5bb350c72ec06dc0ae
kokomo <interse@naver.com>
2020-10-29 13:25:54 +0900

    new View

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -2,7 +2,7 @@
         <h1>글작성</h1>
         <form action="{{ route('articles.store') }}" method="post">
             {!! csrf_field() !!}
-            <div class="form-group {{ $error->has('title') ? 'has-error' : ''}}">
+            <div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
                 <label for="title">제목</label>
                 <input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control" />
                 {!!$errors->first('title' , '<span class="form-error">:message</span>')!!}
