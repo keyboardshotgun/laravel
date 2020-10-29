1549273f35bfd6f2f40110127fe9acec2387952d
kokomo <interse@naver.com>
2020-10-29 13:26:44 +0900

    new View

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -7,7 +7,7 @@
                 <input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control" />
                 {!!$errors->first('title' , '<span class="form-error">:message</span>')!!}
             </div>
-            <div class="form-group {{ $error->has('content') ? 'has-error' : ''}}">
+            <div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
                 <label for="content">본문</label>
                 <textarea name="content" id="content" rows="10" class="form-control">
                     {{ old('content') }}
