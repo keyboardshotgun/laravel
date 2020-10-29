61bcc6de85848cb17427887d75f31eca82917672
상현 <interse@naver.com>
2020-10-29 10:42:21 +0900

    add constraint

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+    // protected $table = 'persons'; // 첫 줄에 처리해줘야 함.
+    // public $timestamps = false; // 라라벨 tinker는 삽입시 데이터 컬럼명이 updated_at 이여야 하는데 , 그러지 않은 경우위해
+    protected $fillable = ['title','content'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+        // User model과 관계되어 있다고 표기 , 반대로 User 에는 Articles를 가지고 있다고 표기해줘야 함.
+    }
 }
